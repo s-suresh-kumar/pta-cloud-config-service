976efccdb8a2a7cfb8e3af7e90cfa9795cea233c
Suresh Kumar <suresh.uncbc.coding@gmail.com>
2021-06-22 17:09:36 -0400

    Initial oommit

---
+++ src/test/java/com/cognizant/ptacloudconfigservice/PtaCloudConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.ptacloudconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtaCloudConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
